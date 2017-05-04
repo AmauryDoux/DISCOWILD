7bee7cd9fc07639a9a1e13f634d8a67346f1fd0e
AmauryDoux <amaury.doux@gmail.com>
2017-05-04 23:09:04 +0200

    updates on the artist page

--- views/artist/artist.js
+++ views/artist/artist.js
@@ -10,18 +10,22 @@ app.component("artist", {
 });
 
 function Artist($scope, $resource) {
-    var request = $resource('https://api.discogs.com/artists/976494',AUTH); // On initialise la variable avec un resource sur l'url
+    var request = $resource('https://api.discogs.com/artists/976494', AUTH); // On initialise la variable avec un resource sur l'url
 
     request.get().$promise.then(function(artist) { // Dans Data l'objet JSON
-    console.log(artist);
+        //console.log(artist);
         $scope.artistName = artist.name;
         $scope.artistProfile = artist.profile;
         $scope.artistImg = artist.images[0].uri150;
         $scope.artistSites = artist.urls;
         $scope.artistMembers = artist.members;
+
+
     });
+    request = $resource('https://api.discogs.com/artists/976494/releases', AUTH); // On initialise la variable avec un resource sur l'url
+    request.get().$promise.then(function(releases) { // Dans Data l'objet JSON
+        $scope.releasesArr = releases.releases;
+        console.log(releases);
+    });
+
 }
-
-
-
-//console.log("Hello " + artist.name); // On peut accéder aux propriétes de l'objet
