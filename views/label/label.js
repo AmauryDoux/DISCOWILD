c0d6e446257423b74afe18f76b89e73274a952c1
hayliblink <zoepagny@gmail.com>
2017-05-04 23:08:33 +0200

    le style avant tout

--- views/label/label.js
+++ views/label/label.js
@@ -11,6 +11,13 @@ app.component("label", {
 function Label($scope, $resource) {
     var request = $resource('https://api.discogs.com/labels/42701'); // On initialise la variable avec un resource sur l'url
     request.get().$promise.then(function(label) { // Dans Data l'objet JSON
-        console.log("Hello " + label.name); // On peut accéder aux propriétes de l'objet
+        console.log(label); // On peut accéder aux propriétes de l'objet
+        $scope.labelName = label.name;
+        $scope.labelProfile = label.profile;
+        $scope.labelImg = label.images[0].uri150;
+        $scope.artistSites = artist.urls;
+        $scope.artistMembers = artist.members;
     });
 }
+
+
