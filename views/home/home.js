e37e09d024ef5b7d82dddaaf9e0f5b97d6da15e1
Wiskyt <samy.bouamama@hotmail.fr>
2017-05-04 15:25:17 +0200

    Premiere requete :)

--- views/home/home.js
+++ views/home/home.js
@@ -1,14 +1,16 @@
 "use strict";
 
-angular.module("discowild", ["ngResource"]).component("home", {
+var app = angular.module("discowild");
+
+app.component("home", {
 
     templateUrl: "views/home/home.html",
     controller: Home
 });
 
-function Home() {
-    console.log("Hello");
-    var test = $resource('https://api.discogs.com/artist/2159610');
-
-    console.log(test.get());
+function Home($scope, $resource) {
+    var artiste = $resource('https://api.discogs.com/artists/2159610'); // On initialise la variable avec un resource sur l'url
+    test.get().$promise.then(function(data) { // Dans Data l'objet JSON
+        console.log("Hello " + data.name); // On peut accéder aux propriétes de l'objet
+    });
 }
